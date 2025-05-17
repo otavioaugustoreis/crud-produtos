89d709a43e53cf4016df838fe5b0391a3c6f0baf
oaugusto265@gmail.com <otavio.almeida@pwi.com.br>
2025-05-17 19:25:34 -0300

    feat: construindo o Service

--- src/main/java/Services/Abstractions/IProdutoService.java
+++ src/main/java/Services/Abstractions/IProdutoService.java
@@ -1,4 +1,6 @@
 package Services.Abstractions;
 
-public interface IProdutoService {
+import Entities.ProdutoEntity;
+
+public interface IProdutoService extends  IService<ProdutoEntity> {
 }
--- src/main/java/Services/ProdutoService.java
+++ src/main/java/Services/ProdutoService.java
@@ -1,17 +1,47 @@
 package Services;
 
+import Entities.ProdutoEntity;
 import Repositories.ProdutoRepository;
+import Services.Abstractions.IProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
-public class ProdutoService {
+public class ProdutoService implements IProdutoService {
 
     @Autowired
     private ProdutoRepository produtoRepository;
 
+    @Override
+    public ProdutoEntity save(ProdutoEntity entity) {
+        return produtoRepository.save(entity);
+    }
 
+    @Override
+    public ProdutoEntity update(Long id, ProdutoEntity entity) {
+        return null;
+    }
 
+    @Override
+    public void delete(Long id) {
+        var produto = produtoRepository.findById(id).orElse(null);
 
+        if( produto != null){
+            produtoRepository.delete(produto);
+        }
+    }
+
+    @Override
+    public Optional<ProdutoEntity> findById(Long id) {
+        return produtoRepository.findById(id);
+    }
+
+    @Override
+    public List<ProdutoEntity> findAll() {
+        return produtoRepository.findAll();
+    }
 }
